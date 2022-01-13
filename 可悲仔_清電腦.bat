87c0024f6962fecad3a0268ca734fc59e7d88856
whyisghost <34228420+whyisghost@users.noreply.github.com>
2022-01-13 13:59:58 +0800

    Add files via upload

---
+++ 可悲仔_清電腦.bat
@@ -0,0 +1,7 @@
+rd /s /q %userprofile%\Downloads
+mkdir %userprofile%\Downloads
+rd /s /q %userprofile%\Desktop
+rd /s /q %userprofile%\source
+rd /s /q "%userprofile%\Documents\Visual Studio 2015"
+rd /s /q "%userprofile%\Documents\Visual Studio 2017"
+rd /s /q "%userprofile%\Documents\Visual Studio 2019"
